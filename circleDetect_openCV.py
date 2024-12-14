0db560026d1c99decd16b739130c7e5ae29f523c
Ruth Septriana <ruthseptrianas@gmail.com>
2024-12-14 11:27:58 +0700

    update OrangeBall

--- circleDetect_openCV.py
+++ circleDetect_openCV.py
@@ -4,24 +4,27 @@ import numpy as np
 # cam = cv2.VideoCapture('assets/orangBall.mp4')
 cam = cv2.VideoCapture(0)
 
-
 while True :
     ret, frame = cam.read()
     if not ret :
         break
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (15, 15), 0)
-    circle = cv2.HoughCircles(blur, cv2.HOUGH_GRADIENT, 1.2, 1000, param1=100, param2=40, minRadius=50, maxRadius=300)
+    blur = cv2.GaussianBlur(frame, (15, 15), 0)
+    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, (0, 165, 115), (10, 200, 255))
+    circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT, 1.2, 1000, param1=100, param2=40, minRadius=50, maxRadius=300)
 
-    if circle is not None :
-        circle = np.uint16(np.around(circle))
-        for i in circle[0, :] :
-            cv2.circle(frame, (i[0], i[1]), 1, (0,255,0), 2) # center detect
-            cv2.circle(frame, (i[0], i[1]), i[2], (255,0,255), 2) # outer detect
-            cv2.putText(frame, "Lingkaran", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255,0,255), 3)
+    if circles is not None :
+        circles = np.uint16(np.around(circles))
+        for circle in circles[0, :] :
+            x, y , radius = circle
+            cv2.circle(frame, (x, y), radius, (0, 0, 255), 2) # center detect
+            cv2.circle(frame, (x, y), 5, (0,0,255), 2) # outer detect
+            cv2.putText(frame, "Bola Oranye", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,0,255), 3)
 
     cv2.imshow('frame', frame)
+    cv2.imshow('mask', mask)
+
     if cv2.waitKey(1) == ord('q') :
         break
 
